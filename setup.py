d57bb48b6dc0709053006eb7c92589aa0648617e
Daniel Cogswell <danielcogswell@ses.ai>
2022-08-03 12:10:06 -0400

    Initial commandline file conversion tool.

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setuptools.setup(
     author_email='danielcogswell@ses.ai',
     url='https://github.com/d-cogswell/NewareNDA',
     packages=['NewareNDA'],
+    scripts=['bin/NewareNDA'],
     install_requires=['pandas'],
     python_requires='>=3.6',
     classifiers=[
