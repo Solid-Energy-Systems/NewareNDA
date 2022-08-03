647cb9dedaa7f8dd3fc7197fca4544991f193a86
Daniel Cogswell <danielcogswell@ses.ai>
2022-08-03 11:50:47 -0400

    Converts NewareNDA from a module to a package.

--- setup.py
+++ setup.py
@@ -4,19 +4,24 @@
 
 import setuptools
 
+version = {}
+with open('NewareNDA/version.py', 'r', encoding='utf-8') as fh:
+    exec(fh.read(), version)
+__version__ = version['__version__']
+
 with open('README.md', 'r', encoding='utf-8') as fh:
     long_description = fh.read()
 
 setuptools.setup(
     name='NewareNDA',
-    version='v2022.08.01',
+    version=__version__,
     description='Neware nda binary file reader.',
     long_description=long_description,
     long_description_content_type="text/markdown",
     author='Daniel Cogswell',
     author_email='danielcogswell@ses.ai',
     url='https://github.com/d-cogswell/NewareNDA',
-    py_modules=['NewareNDA'],
+    packages=['NewareNDA'],
     install_requires=['pandas'],
     python_requires='>=3.6',
     classifiers=[
