00c274390d53bea74a869e765f365d870c211c2a
Daniel Cogswell <danielcogswell@ses.ai>
2023-05-02 14:19:38 -0400

    Adds commandline flags for nda and reference directories.

---
+++ tests/conftest.py
@@ -0,0 +1,29 @@
+import os.path as osp
+import glob
+
+
+def pytest_addoption(parser):
+    parser.addoption(
+        "--ndaDir",
+        action="store",
+        default=osp.join(osp.dirname(osp.abspath(__file__)), 'nda'),
+        help="The directory with the modified test results"
+    )
+    parser.addoption(
+        "--refDir",
+        action="store",
+        default=osp.join(osp.dirname(osp.abspath(__file__)), 'reference'),
+        help="The directory with the base test results"
+    )
+
+
+def pytest_generate_tests(metafunc):
+    nda_dir = metafunc.config.getoption('--ndaDir')
+    ref_dir = metafunc.config.getoption('--refDir')
+
+    # Generate list of files to compare
+    nda_files = glob.glob(nda_dir + '/**/*.nda', recursive=True)
+    ref_files = [osp.join(ref_dir, f"{osp.splitext(osp.basename(f))[0]}.ftr")
+                 for f in nda_files]
+
+    metafunc.parametrize("nda_file, ref_file", list(zip(nda_files, ref_files)))
--- tests/test_NewareNDA.py
+++ tests/test_NewareNDA.py
@@ -1,20 +1,7 @@
-import os
-import glob
 import pandas as pd
 import NewareNDA
 
-import pytest
 
-nda_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'nda')
-ref_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'reference')
-
-# Generate list of files to compare
-nda_files = glob.glob(nda_dir + '/**/*.nda', recursive=True)
-ref_files = [os.path.join(ref_dir, f"{os.path.splitext(os.path.basename(f))[0]}.ftr") for f in nda_files]
-testdata = list(zip(nda_files, ref_files))
-
-
-@pytest.mark.parametrize("nda_file, ref_file", testdata)
 def test_NDA(nda_file, ref_file):
     df = NewareNDA.read(nda_file)
     ref_df = pd.read_feather(ref_file)
