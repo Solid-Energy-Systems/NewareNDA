51a9c24f9cd9e253981f709fe4d87a2f34ae03ee
Daniel Cogswell <danielcogswell@ses.ai>
2024-05-07 16:15:42 -0400

    Check that footer is found before logging info.

--- NewareNDA/NewareNDA.py
+++ NewareNDA/NewareNDA.py
@@ -186,20 +186,21 @@ def _read_nda_130(mm):
     # Find footer data block
     mm.seek(1024)
     footer = mm.rfind(b'\x06\x00\xf0\x1d\x81\x00\x03\x00\x61\x90\x71\x90\x02\x7f\xff\x00')
-    
-    mm.seek(footer+16)
-    bytes = mm.read(499)
-    
-    # Get the active mass
-    [active_mass] = struct.unpack('<d', bytes[-8:])
-    logging.info(f"Active mass: {active_mass} mg")
-    
-    # Get the remarks
-    remarks = bytes[363:491].decode('ASCII')
-    
-    # Clean null characters
-    remarks = remarks.replace(chr(0), '').strip()
-    logging.info(f"Remarks: {remarks}")
+
+    if footer != -1:
+        mm.seek(footer+16)
+        bytes = mm.read(499)
+
+        # Get the active mass
+        [active_mass] = struct.unpack('<d', bytes[-8:])
+        logging.info(f"Active mass: {active_mass} mg")
+
+        # Get the remarks
+        remarks = bytes[363:491].decode('ASCII')
+
+        # Clean null characters
+        remarks = remarks.replace(chr(0), '').strip()
+        logging.info(f"Remarks: {remarks}")
 
     return output, aux
 
